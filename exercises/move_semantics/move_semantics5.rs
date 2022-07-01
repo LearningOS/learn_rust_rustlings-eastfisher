5ffa555368e0c133faf0fda3ab7242f1ffc4f177
eastfisher <eastfisher72@gmail.com>
2022-07-01 16:02:24 +0800

    finish 29 exercises

--- exercises/move_semantics/move_semantics5.rs
+++ exercises/move_semantics/move_semantics5.rs
@@ -3,13 +3,12 @@
 // adding, changing or removing any of them.
 // Execute `rustlings hint move_semantics5` for hints :)
 
-// I AM NOT DONE
 
 fn main() {
     let mut x = 100;
     let y = &mut x;
-    let z = &mut x;
     *y += 100;
+    let z = &mut x; // NLL起作用了, 即使y没有离开作用域, 也不会与z冲突
     *z += 1000;
     assert_eq!(x, 1200);
 }
